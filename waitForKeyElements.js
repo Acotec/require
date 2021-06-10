dfcf1871fa05915a7b779f9c721b30b4c5f49f36
HARFHO <Harfho77@gmail.com>
2021-06-10 06:52:23 -0600

    Update waitForKeyElements.js
    change the whole code from
     https://gist.github.com/BrockA/2625891
    to
     https://github.com/CoeJoder/waitForKeyElements.js

--- waitForKeyElements.js
+++ waitForKeyElements.js
@@ -1,17 +1,62 @@
-/*--- waitForKeyElements():  A utility function, for Greasemonkey scripts,
-    that detects and handles AJAXed content.
+/**
+ * A utility function for userscripts that detects and handles AJAXed content.
+ *
+ * @example
+ * waitForKeyElements("div.comments", (element) => {
+ *   element.innerHTML = "This text inserted by waitForKeyElements().";
+ * });
+ *
+ * waitForKeyElements(() => {
+ *   const iframe = document.querySelector('iframe');
+ *   if (iframe) {
+ *     const iframeDoc = iframe.contentDocument || iframe.contentWindow.document;
+ *     return iframeDoc.querySelectorAll("div.comments");
+ *   }
+ *   return null;
+ * }, callbackFunc);
+ *
+ * @param {(string|function)} selectorOrFunction - The selector string or function.
+ * @param {function}          callback           - The callback function; takes a single DOM element as parameter.
+ *                                                 If returns true, element will be processed again on subsequent iterations.
+ * @param {boolean}           [waitOnce=true]    - Whether to stop after the first elements are found.
+ * @param {number}            [interval=300]     - The time (ms) to wait between iterations.
+ * @param {number}            [maxIntervals=-1]  - The max number of intervals to run (negative number for unlimited).
+ */
+function waitForKeyElements(selectorOrFunction, callback, waitOnce, interval, maxIntervals) {
+    if (typeof waitOnce === "undefined") {
+        waitOnce = true;
+    }
+    if (typeof interval === "undefined") {
+        interval = 300;
+    }
+    if (typeof maxIntervals === "undefined") {
+        maxIntervals = -1;
+    }
+    var targetNodes = (typeof selectorOrFunction === "function")
+            ? selectorOrFunction()
+            : document.querySelectorAll(selectorOrFunction);
 
-    Usage example:
-      in your script load the function with ;
-      // @require  https://github.com/Acotec/require/raw/master/waitForKeyElements.js
+    var targetsFound = targetNodes && targetNodes.length > 0;
+    if (targetsFound) {
+        targetNodes.forEach(function(targetNode) {
+            var attrAlreadyFound = "data-userscript-alreadyFound";
+            var alreadyFound = targetNode.getAttribute(attrAlreadyFound) || false;
+            if (!alreadyFound) {
+                var cancelFound = callback(targetNode);
+                if (cancelFound) {
+                    targetsFound = false;
+                }
+                else {
+                    targetNode.setAttribute(attrAlreadyFound, true);
+                }
+            }
+        });
+    }
 
-        waitForKeyElements ("div.comments", commentCallbackFunction);
-
-        //--- Page-specific function to do what we want when the node is found.
-        function commentCallbackFunction (jNode) {
-            jNode.text ("This comment changed by waitForKeyElements().");
-        }
-
-    IMPORTANT: This function requires your script to have loaded jQuery.
-*/
-function waitForKeyElements(e,t,a,n){var o,r;(o=void 0===n?$(e):$(n).contents().find(e))&&o.length>0?(r=!0,o.each(function(){var e=$(this);e.data("alreadyFound")||!1||(t(e)?r=!1:e.data("alreadyFound",!0))})):r=!1;var l=waitForKeyElements.controlObj||{},i=e.replace(/[^\w]/g,"_"),c=l[i];r&&a&&c?(clearInterval(c),delete l[i]):c||(c=setInterval(function(){waitForKeyElements(e,t,a,n)},0),l[i]=c),waitForKeyElements.controlObj=l}
+    if (maxIntervals !== 0 && !(targetsFound && waitOnce)) {
+        maxIntervals -= 1;
+        setTimeout(function() {
+            waitForKeyElements(selectorOrFunction, callback, waitOnce, interval, maxIntervals);
+        }, interval);
+    }
+}
