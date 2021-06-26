520cd7f9e2fec836e9b91714744ceb9aa41f7137
HARFHO <Harfho77@gmail.com>
2021-06-26 14:24:15 +0400

    Update delay.js

--- delay.js
+++ delay.js
@@ -1,4 +1,4 @@
 var flag=new RegExp("(express-cut.ovh)|(bitlinks.pw)|(neonlink.net)|(faucet.100count.net)|(bitcoinly.in)|(kiiw.icu)|(adbull.me)|(owllink.net)|(pingit.im)|\
                      (cashurl.in)|(adshort.co)|(aii.sh)|(riful.com)|(fc.lc)");
 function sleep(e){for(var i=(new Date).getTime();new Date<i+1e3*e;);return 0}
-flag.test(window.location.host)&&sleep(20);
+flag.test(window.location.host)&&alert('worlinh');
